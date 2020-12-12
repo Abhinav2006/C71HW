5c52d43bcb16e8f1b5fe45344136df7fc5aae460
Abhinav2006 <62171671+Abhinav2006@users.noreply.github.com>
2020-12-12 12:12:07 +0530

    Add files via upload

---
+++ screens/WriteStoryScreen.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Text, View, Header, TextInput, TouchableOpacity, KeyboardAvoidingView, StyleSheet } from 'react-native';
+import db from '../config';
+import firebase from 'firebase';
+
+export default class WriteStoryScreen extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            title: "",
+            author: "",
+            story: ""
+        }
+    }
+    submitStory =async()=>{
+        db.collection('Story_Info').add({
+            'story_title': this.state.title,
+            'story_author': this.state.author,
+            'story_story': this.state.story
+        })
+    }
+    render(){
+        return(
+            <View>
+                <TouchableOpacity
+                style = {styles.submitButton}
+                onPress = {this.submitStory}>
+                    <Text style = {{justifyContent: 'center', alignSelf: 'center'}}>Submit</Text>
+                </TouchableOpacity>
+                <Text style = {{marginLeft: 30, marginTop:10}}>Write a story!</Text>
+                <KeyboardAvoidingView>
+                <TextInput style = {{borderWidth: 1.5, width: 300, height: 30, marginLeft: 30, marginTop: 5}}
+                placeholder = {"Story's Title"}
+                onChangeText = {(text)=>this.setState({title:text})}/>
+                <TextInput style = {{borderWidth: 1.5, width: 300, height: 30, marginLeft: 30, marginTop: 5}}
+                placeholder = {"Author"}
+                onChangeText = {(text)=>this.setState({author:text})}/>
+                <TextInput style = {{borderWidth: 1.5, width: 300, height: 400, marginLeft: 30, marginTop: 5}}
+                multiline
+                placeholder = {"Story"}
+                onChangeText = {(text)=>this.setState({story:text})}/>
+                </KeyboardAvoidingView>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    submitButton:{
+        backgroundColor: 'green',
+        alignSelf: 'center',
+        alignContent: 'center',
+        justifyContent: 'center',
+        marginTop: 20,
+        width: 100,
+        height: 50,
+        padding: 10,
+        borderRadius: 20
+    }
+})
